1b4be65e737a891eab081cfd255c5e21c270dcea
thejayden <tanjiadian.tjd@gmail.com>
2020-07-03 18:13:25 +0800

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,13 +4,23 @@ import { DISHES } from "../shared/dishes";
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
+            selectedDish: null,
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
@@ -19,18 +29,26 @@ class Main extends Component {
     }
 
     render() {
+        const HomePage = () => {
+            return(
+                <Home 
+                    dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
+            );
+          }
+
         return (
             <div className="App">
-                <Header/>
-                <Menu dishes={this.state.dishes} 
-                    onClick={(dishId) => this.onDishSelect(dishId)}/>
-                <DishDetail 
-                //for each dish in dishes array, find dish id that matches the one that is selected(clicked)
-                //calls subarry within dishes array which contains dish.id as one of ite elements 
-                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-                {/* 
-        <DishDetail dishes={this.state.dishes} /> */}
-                <Footer/>
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)}/>} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Redirect to="/home" />
+                </Switch>
+                <Footer />
             </div>
         );
     }
