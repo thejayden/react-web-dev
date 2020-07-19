14a61d34baeec3127b32cf5bef868381f53ad485
thejayden <tanjiadian.tjd@gmail.com>
2020-07-19 22:20:35 +0800

    Fetch from Server

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,12 +8,12 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { addComment, fetchDishes } from '../redux/ActionCreators';
+import { addComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
 
 
 const mapStateToProps = state => {
-    return{
+    return {
         dishes: state.dishes,
         comments: state.comments,
         promotions: state.promotions,
@@ -22,10 +22,11 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = dispatch => ({
-
     addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
-    fetchDishes: () => { dispatch(fetchDishes())},
-    resetFeedbackForm: () => { dispatch(actions.reset('feedback'))}
+    fetchDishes: () => { dispatch(fetchDishes()) },
+    resetFeedbackForm: () => { dispatch(actions.reset('feedback')) },
+    fetchComments: () => dispatch(fetchComments()),
+    fetchPromos: () => dispatch(fetchPromos())
 });
 
 class Main extends Component {
@@ -33,10 +34,13 @@ class Main extends Component {
         super(props);
 
     }
-
+    //when main component is mounted, then fetch all these from server
     componentDidMount() {
         this.props.fetchDishes();
+        this.props.fetchComments();
+        this.props.fetchPromos();
     }
+
 
     render() {
         const HomePage = () => {
@@ -44,19 +48,23 @@ class Main extends Component {
                 <Home
                     dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
                     dishesLoading={this.props.dishes.isLoading}
-                    dishesErrMess={this.props.dishes.errMess}
-                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    dishErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.promotions.filter((promo) => promo.featured)[0]}
+                    promoLoading={this.props.promotions.isLoading}
+                    promoErrMess={this.props.promotions.errMess}
                     leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
+
             );
-        }
+        };
 
         const DishWithId = ({ match }) => {
             return (
                 <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
                     isLoading={this.props.dishes.isLoading}
                     errMess={this.props.dishes.errMess}
-                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    commentsErrMess={this.props.comments.errMess}
                     addComment={this.props.addComment}
                 />
             );
@@ -70,7 +78,7 @@ class Main extends Component {
                     <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
                     <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
-                    <Route path='/aboutus' component={() => <About leaders={this.props.leaders}/>}/>
+                    <Route path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,16 +1,26 @@
-import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
-export const Comments = (state = COMMENTS, action) => {
+export const Comments = (state = {
+    errMess: null,
+    comments: []
+},
+    action) => {
     switch (action.type) {
+        case ActionTypes.ADD_COMMENTS:
+            return { ...state, errMess: null, comments: action.payload };
+
+        case ActionTypes.COMMENTS_FAILED:
+            return { ...state, errMess: action.payload, comments:[] };
+
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
-            comment.id = state.length;
+            //note the sate.comments
+            comment.id = state.comments.length;
             comment.date = new Date().toISOString();
-            console.log("Comment: ", comment);
-            return state.concat(comment);
+            //state shape has changed, so use ...state
+            return { ...state, comments: state.comments.concat(comment) };
 
         default:
-          return state;
-      }
+            return state;
+    }
 };
